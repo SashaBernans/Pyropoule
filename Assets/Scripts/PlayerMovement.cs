9d44aa786ba2a69bedcaee4c041091e32b3b2b7b
sasha <sabern4@gmail.com>
2023-11-19 21:09:39 -0500

    Run and jump animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
 
+    private Animator animator;
+    private bool hasJumped = false;
+
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,6 +32,12 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+            // Trigger the jump animation
+            animator.SetTrigger("Jump");
+
+            // Set a flag to prevent multiple jump animations
+            hasJumped = true;
         }
     }
 
@@ -37,5 +47,12 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 movement = new Vector2(horizontalInput * speed, rb.velocity.y);
         rb.velocity = movement;
+    }
+
+    // You may want to reset the jump flag when the player lands or finishes the jump animation
+    public void OnLanding()
+    {
+        // Reset the jump flag
+        hasJumped = false;
     }
 }
